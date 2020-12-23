db9acc29872bc14d15a271fb295d3cbf0ad389a1
LilyFG <l.t.fitzgibbon@reading.ac.uk>
2020-12-23 16:21:04 +0000

    Add emergency end function after 30 seconds if speech hasn't ended by itself.

--- js/speech.js
+++ js/speech.js
@@ -1,7 +1,8 @@
 var synth = window.speechSynthesis;
 
 function speak(text, enable = true, end_func = null){
-
+    var ended = F
+    
     if (synth.speaking) {
       speechSynthesis.cancel()
         //console.error('speechSynthesis.speaking');
@@ -10,7 +11,8 @@ function speak(text, enable = true, end_func = null){
 
     var utterThis = new SpeechSynthesisUtterance(text);
 
-    utterThis.onend = function (event) {
+    var onend = function (event) {
+        ended = T
         if (enable) {
           console.log(enable);
           $(enable).css('display', "block"); // use the next argument to give the id of the next button
@@ -22,6 +24,17 @@ function speak(text, enable = true, end_func = null){
           end_func() // use the end_func argument to run a function on end
         }
     }
+    
+    utterThis.onend = onend()
+    
+    setTimeout(
+        function(){
+            if(!ended){
+                console.error('Speech synthesis failed to end properly, backup procedure called.');
+                onend()
+            }                
+        }, 30000)
+    
 
     utterThis.onerror = function (event) {
         console.error('SpeechSynthesisUtterance.onerror '+event.error);
