b8ceedc553bce2bb9f8d1156143760fbad603690
ZoeJRyan <55541447+ZoeJRyan@users.noreply.github.com>
2021-11-25 15:00:55 +0000

    Update ID_EC.js
    Adding response scale wording and all questions for ID_EC.

--- ID_EC.js
+++ ID_EC.js
@@ -1,17 +1,23 @@
 var likert_scale = [
-  "Strongly Disagree",
-  "Disagree",
-  "Neutral",
-  "Agree",
-  "Strongly Agree"
+  "Almost Never",
+  "Sometimes",
+  "Often",
+  "Almost Always"
 ];
 
 var trial = {
   type: jsPsychSurveyLikert,
   questions: [
-    {prompt: "I like vegetables.", name: 'Vegetables', labels: likert_scale},
-    {prompt: "I like fruit.", name: 'Fruit', labels: likert_scale},
-    {prompt: "I like meat.", name: 'Meat', labels: likert_scale},
+    {prompt: "Enjoy exploring new ideas", labels: likert_scale},
+    {prompt: "Hours on a problem because I can’t rest without answer", labels: likert_scale},
+    {prompt: "Find it fascinating to learn new information", labels: likert_scale},
+    {prompt: "Brood for a long time to solve problem", labels: likert_scale},
+    {prompt: "Enjoy learning about subjects that are unfamiliar to me", labels: likert_scale},
+    {prompt: "Conceptual problems keep me awake thinking", labels: likert_scale},
+    {prompt: "Enjoy discussing abstract concepts", labels: likert_scale},
+    {prompt: "Frustrated if I can’t figure out problem, so I work harder", labels: likert_scale},
+    {prompt: "Learn something new, like to find out more about it", labels: likert_scale},
+    {prompt: "Work like a fiend at problems that I feel must be solved", labels: likert_scale},
   ],
-  randomize_question_order: true
+  randomize_question_order: false
 };
