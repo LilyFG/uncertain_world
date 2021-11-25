0e1866ebff5176f364c6420b5b658f387cd8e226
LilyFG <l.t.fitzgibbon@reading.ac.uk>
2021-11-25 17:00:14 +0000

    Preamble placeholder and required
    @ZoeJRyan I've added some placeholder text for the preamble instructions and included a parameter that makes a response for each question required. Please could you add the instructions here and do the same for the IUS and also add the required: true to the IUS items. Sorry for not thinking of this earlier!

--- ID_EC.js
+++ ID_EC.js
@@ -6,18 +6,19 @@ var likert_scale = [
 ];
 
 var trial = {
-  type: jsPsychSurveyLikert,
+  type: "survey-likert",
+  preamble: "<h1>SURVEY TITLE GOES HERE</h1><div>Survey preamble text goes here</div>",
   questions: [
-    {prompt: "Enjoy exploring new ideas", labels: likert_scale},
-    {prompt: "Hours on a problem because I can’t rest without answer", labels: likert_scale},
-    {prompt: "Find it fascinating to learn new information", labels: likert_scale},
-    {prompt: "Brood for a long time to solve problem", labels: likert_scale},
-    {prompt: "Enjoy learning about subjects that are unfamiliar to me", labels: likert_scale},
-    {prompt: "Conceptual problems keep me awake thinking", labels: likert_scale},
-    {prompt: "Enjoy discussing abstract concepts", labels: likert_scale},
-    {prompt: "Frustrated if I can’t figure out problem, so I work harder", labels: likert_scale},
-    {prompt: "Learn something new, like to find out more about it", labels: likert_scale},
-    {prompt: "Work like a fiend at problems that I feel must be solved", labels: likert_scale},
+    {prompt: "Enjoy exploring new ideas", labels: likert_scale, required: true},
+    {prompt: "Hours on a problem because I can’t rest without answer", labels: likert_scale, required: true},
+    {prompt: "Find it fascinating to learn new information", labels: likert_scale, required: true},
+    {prompt: "Brood for a long time to solve problem", labels: likert_scale, required: true},
+    {prompt: "Enjoy learning about subjects that are unfamiliar to me", labels: likert_scale, required: true},
+    {prompt: "Conceptual problems keep me awake thinking", labels: likert_scale, required: true},
+    {prompt: "Enjoy discussing abstract concepts", labels: likert_scale, required: true},
+    {prompt: "Frustrated if I can’t figure out problem, so I work harder", labels: likert_scale, required: true},
+    {prompt: "Learn something new, like to find out more about it", labels: likert_scale, required: true},
+    {prompt: "Work like a fiend at problems that I feel must be solved", labels: likert_scale, required: true},
   ],
   randomize_question_order: false
 };
